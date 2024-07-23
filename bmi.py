b0544affe355753f0a776c5f5d2ba74b3450c921
Dhruv Shah <99525371+shahhdhruv@users.noreply.github.com>
2024-07-24 04:17:16 +0530

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,7 @@
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+
+bmi = int(weight)/float(height)**2
+print(int(bmi))
